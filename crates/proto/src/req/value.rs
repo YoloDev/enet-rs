0d697aaabd869c3f21496f0350b46e5d3f8e768c
Aleksander Heintz <alxandr@alxandr.me>
2021-10-28 22:11:41 +0200

    test(proto): add more tests

--- crates/proto/src/req/value.rs
+++ crates/proto/src/req/value.rs
@@ -71,3 +71,112 @@ pub enum ClickDuration {
   Short,
   Long,
 }
+
+#[cfg(test)]
+mod tests {
+  use serde_test::{assert_ser_tokens, Token};
+
+  use super::*;
+
+  fn item(value: SetValue) -> ItemSetValue {
+    ItemSetValue { value, number: 1 }
+  }
+
+  #[test]
+  fn item_set_value_on_short() {
+    assert_ser_tokens(
+      &item(SetValue::On(ClickDuration::Short)),
+      &[
+        Token::Map { len: None },
+        Token::Str("STATE"),
+        Token::Str("ON"),
+        Token::Str("NUMBER"),
+        Token::U32(1),
+        Token::MapEnd,
+      ],
+    )
+  }
+
+  #[test]
+  fn item_set_value_off_short() {
+    assert_ser_tokens(
+      &item(SetValue::Off(ClickDuration::Short)),
+      &[
+        Token::Map { len: None },
+        Token::Str("STATE"),
+        Token::Str("OFF"),
+        Token::Str("NUMBER"),
+        Token::U32(1),
+        Token::MapEnd,
+      ],
+    )
+  }
+
+  #[test]
+  fn item_set_value_on_long() {
+    assert_ser_tokens(
+      &item(SetValue::On(ClickDuration::Long)),
+      &[
+        Token::Map { len: None },
+        Token::Str("STATE"),
+        Token::Str("ON"),
+        Token::Str("LONG_CLICK"),
+        Token::Str("ON"),
+        Token::Str("NUMBER"),
+        Token::U32(1),
+        Token::MapEnd,
+      ],
+    )
+  }
+
+  #[test]
+  fn item_set_value_off_long() {
+    assert_ser_tokens(
+      &item(SetValue::Off(ClickDuration::Long)),
+      &[
+        Token::Map { len: None },
+        Token::Str("STATE"),
+        Token::Str("OFF"),
+        Token::Str("LONG_CLICK"),
+        Token::Str("ON"),
+        Token::Str("NUMBER"),
+        Token::U32(1),
+        Token::MapEnd,
+      ],
+    )
+  }
+
+  #[test]
+  fn item_set_value_dim() {
+    assert_ser_tokens(
+      &item(SetValue::Dimm(50)),
+      &[
+        Token::Map { len: None },
+        Token::Str("STATE"),
+        Token::Str("VALUE_DIMM"),
+        Token::Str("VALUE"),
+        Token::U8(50),
+        Token::Str("NUMBER"),
+        Token::U32(1),
+        Token::MapEnd,
+      ],
+    )
+  }
+
+  #[test]
+  fn item_set_value_blinds() {
+    assert_ser_tokens(
+      &item(SetValue::Blinds(50)),
+      &[
+        Token::Map { len: None },
+        Token::Str("STATE"),
+        Token::Str("VALUE_BLINDS"),
+        Token::Str("VALUE"),
+        Token::U8(50),
+        Token::Str("NUMBER"),
+        Token::U32(1),
+        Token::MapEnd,
+      ],
+    )
+  }
+}
